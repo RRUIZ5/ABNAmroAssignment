2eb1f7d0875515d142a37cc0344f144eded59a70
Rodrigo Ruiz Murguía <rodrigo.ruizmurguia@gmail.com>
2025-11-23 16:06:00 +0100

    Initial Commit

---
+++ ABNAmroAssignment/ABNAmroAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABNAmroAssignmentApp.swift
+//  ABNAmroAssignment
+//
+//  Created by Rodrigo Ruiz on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ABNAmroAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABNAmroAssignmentTests/ABNAmroAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABNAmroAssignmentTests.swift
+//  ABNAmroAssignmentTests
+//
+//  Created by Rodrigo Ruiz on 23/11/2025.
+//
+
+import Testing
+@testable import ABNAmroAssignment
+
+struct ABNAmroAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
